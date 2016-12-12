f1d6d50b601142684d3a762e0682ab19c4951bbe
Bianca Rocha <bianca.rocha1711@gmail.com>
2016-12-12 11:38:34 -0300

    atualizacoes no bd

--- teste-login/models/database.js
+++ teste-login/models/database.js
@@ -1,15 +1,12 @@
 const pg = require('pg');
-//const connectionString = process.env.DATABASE_URL || 'postgres://bianca:1234@localhost/postgres';
+const pgp = require('pg-promise')();
 
 const connectionString = process.env.DATABASE_URL || 'postgres://bianca:1234@localhost/postgres';
 const client = new pg.Client(connectionString);
 client.connect();
 
-module.exports = {
-	Banco: function() {
-		return connectionString;
-	}
-}
+var db =pgp(connectionString);
+
 
 function Cria(login, senha) {
 		client.query('INSERT INTO clientes(login, senha) values($1, $2)',
@@ -18,11 +15,13 @@ function Cria(login, senha) {
 }
 
 function Procura(login, senha) {
-	const results = [];
-	const query = client.query('SELECT * FROM Clientes WHERE login = $1 AND senha = $2', [login, senha]);
-	query.on('row',(row) => {
-		results.push(row);
-		return results;
+	db.one("SELECT * FROM clientes WHERE login = $1 AND senha = $2",[login, senha])
+	.then(function(data) {
+		console.log(data.login);
+		console.log(data.senha);
+	})
+	.catch(function(error) {
+		console.log("ERROR", error.message || error);
 	});
 }
 
